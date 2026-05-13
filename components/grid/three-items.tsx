9557ea075c10876abdc463f088854ff792e6926c
Andrew Jennings <andrew@edition3.io>
2026-05-13 18:16:30 -0300

    Fix ThreeItemGrid to work with fewer than 3 products

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -53,15 +53,19 @@ export async function ThreeItemGrid() {
     collection: "hidden-homepage-featured-items",
   });
 
-  if (!homepageItems[0] || !homepageItems[1] || !homepageItems[2]) return null;
+  if (!homepageItems[0]) return null;
 
   const [firstProduct, secondProduct, thirdProduct] = homepageItems;
 
   return (
     <section className="mx-auto grid max-w-(--breakpoint-2xl) gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2 lg:max-h-[calc(100vh-200px)]">
       <ThreeItemGridItem size="full" item={firstProduct} priority={true} />
-      <ThreeItemGridItem size="half" item={secondProduct} priority={true} />
-      <ThreeItemGridItem size="half" item={thirdProduct} />
+      {secondProduct && (
+        <ThreeItemGridItem size="half" item={secondProduct} priority={true} />
+      )}
+      {thirdProduct && (
+        <ThreeItemGridItem size="half" item={thirdProduct} />
+      )}
     </section>
   );
 }
